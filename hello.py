4b1ab9e0358b5ed945a782393a7ac1684a000d20
Cory Spitzer <coryspitzer@gmail.com>
2015-07-27 17:06:05 -0700

    Add explanatory comments

--- hello.py
+++ hello.py
@@ -8,10 +8,24 @@ from pyramid.response import Response
 def hello_world(request):
     return Response('Hello %(name)s!' % request.matchdict)
 
+def goodbye_world(request):
+    return Response('Goodbye %(name)s!' % request.matchdict)
+
 if __name__ == '__main__':
     config = Configurator()
+
+    # add the 'hello' route matching the path '/hello/<any-string-you-like>':
     config.add_route('hello', '/hello/{name}')
+    config.add_route('goodbye', '/goodbye/{name}')
+    # give that route a view with the above defined hello_world:
     config.add_view(hello_world, route_name='hello')
+    config.add_view(goodbye_world, route_name='goodbye')
+
+    # after we finish all the config and setting up the we make a magical
+    # "WSGI application object that can be used by any WSGI server to present
+    # an application to a requestor.":
     app = config.make_wsgi_app()
+
+    # Listen on all TCP interfaces with '0.0.0.0'
     server = make_server('0.0.0.0', 8080, app)
     server.serve_forever()
